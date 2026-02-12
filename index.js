04b82ebbcb3135147700812a9d951a8f1e95feaf
Vinayachandran Viswanathan <vinayachandranv@gmail.com>
2026-02-12 19:55:31 +0530

    Test CI/CD auto deploy with console log

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const pool = new Pool({
 });
 
 app.get("/", (req, res) => {
-  res.send("Family Tree API is running 🚀 (auto-deployed)");
+  res.send("Testing");
 });
 
 app.get("/api/family/roots", async (req, res) => {
